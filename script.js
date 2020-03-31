500231a5e72975b2ced7d71dcefcf54bf3e2d131
--global <johndoe@example.com>
2020-03-31 12:50:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Odesa,ua&appid=2221514d1a93d2db9db522461d4f7b91')
+    .then(function (resp) { return resp.json() })
+    .then(function (data) {
+        console.log(data);
+        document.querySelector('.package-name').textContent = data.name;
+        document.querySelector('.price').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+        document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
+        //https://openweathermap.org/img/wn/02d@2x.png
+        document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+    })
+    .catch(function () {
+        // catch any errors
+    });
